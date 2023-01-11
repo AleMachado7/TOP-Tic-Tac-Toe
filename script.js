75ec211b258aa8ddaaa35c8e49601c4f40feca8d
Alexandre <aleaxe7@gmail.com>
2023-01-10 21:38:46 -0300

    Add more styling, add function to dinamic display gameboard, add function to render player marks

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
 // create game board module
 const gameBoard = (() => {
-  const marks = ["X", "X", "O", "X", "O", "O", "X", "O", "X"];
+  const boardArray = new Array(9);
+  const boardField = document.querySelector(".game-board");
 
-  const setMark = (mark) => {
-    marks.push(mark);
-  };
+  // create the board fields
+  for (let i = 0; i < boardArray.length; i++) {
+    const field = document.createElement("div");
+    field.classList.add("board-field");
+    field.setAttribute("data-index", `${i}`);
+    boardField.appendChild(field);
+  }
 
-  const drawBoard = () => {
-    const board = document.querySelector(".game-board");
-    marks.forEach((mark) => {
-      const newMark = document.createElement("div");
-      newMark.textContent = mark;
-      newMark.classList.add("mark");
-      board.appendChild(newMark);
+  // display the markers
+  const displayMarkers = () => {
+    const fields = document.querySelectorAll(".board-field");
+    fields.forEach((field) => {
+      const fieldIndex = field.getAttribute("data-index");
+      field.textContent = boardArray[fieldIndex];
     });
   };
 
-  return { setMark, drawBoard };
+  const setMarker = (playerMark) => {
+    if (boardArray[fieldIndex] !== undefined) {
+      console.log("This field is already taken");
+    }
+  };
+
+  return { setMarker, displayMarkers };
 })();
+
+const player = (playerName, playerMarker) => {
+  const setMarker = () => {
+    const fields = document.querySelectorAll(".board-field");
+  };
+
+  return { playerName, setMarker };
+};
