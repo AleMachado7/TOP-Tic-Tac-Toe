ec7efc2fa293ee28505f519e4d5d686470cd2f93
Alexandre <aleaxe7@gmail.com>
2023-01-19 21:44:56 -0300

    Added function to verify winner

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // create game board module
 const gameBoard = (() => {
-  const boardArray = new Array(9);
+  let boardArray = new Array(9);
   const boardField = document.querySelector(".game-board");
 
   // create the board fields
@@ -10,6 +10,8 @@ const gameBoard = (() => {
     field.setAttribute("data-index", `${i}`);
     boardField.appendChild(field);
   }
+
+  const getBoard = () => boardArray;
 
   // display the markers
   const displayMarkers = () => {
@@ -27,17 +29,59 @@ const gameBoard = (() => {
     }
   };
 
-  return { setMarker };
+  const clearBoard = () => {
+    boardArray = new Array(9);
+    displayMarkers();
+  };
+
+  return { getBoard, setMarker, clearBoard };
 })();
 
 const gameFlow = (() => {
   let currentPlayer = "X";
-  const playerTurn = document.querySelector("#game-info");
-  playerTurn.textContent = `It's player ${currentPlayer} turn.`;
+  const gameInfo = document.querySelector("#game-info");
+  gameInfo.textContent = `It's player ${currentPlayer} turn.`;
 
   const changePlayer = () => {
     currentPlayer = currentPlayer === "X" ? "O" : "X";
-    playerTurn.textContent = `It's player ${currentPlayer} turn.`;
+    gameInfo.textContent = `It's player ${currentPlayer} turn.`;
+  };
+
+  const checkForWinner = () => {
+    let roundWon = false;
+    const board = gameBoard.getBoard();
+    const winConditions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < winConditions.length; i++) {
+      const condition = winConditions[i];
+      const cellA = board[condition[0]];
+      const cellB = board[condition[1]];
+      const cellC = board[condition[2]];
+
+      if (cellA === undefined || cellB === undefined || cellC === undefined) {
+        continue;
+      }
+      if (cellA === cellB && cellB === cellC) {
+        roundWon = true;
+        break;
+      }
+    }
+
+    if (roundWon) {
+      gameInfo.textContent = `${currentPlayer} wins!`;
+    } else if (!board.includes(undefined)) {
+      gameInfo.textContent = "It`s a draw!";
+    } else {
+      changePlayer();
+    }
   };
 
   const fields = document.querySelectorAll(".board-field");
@@ -45,7 +89,7 @@ const gameFlow = (() => {
     field.addEventListener("click", () => {
       const fieldIndex = field.getAttribute("data-index");
       gameBoard.setMarker(currentPlayer, fieldIndex);
-      changePlayer();
+      checkForWinner();
     });
   });
 })();
